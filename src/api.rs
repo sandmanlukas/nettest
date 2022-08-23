5718f6557226a2c7dfcad31f6bd96f590958cc28
Lukas Sandman <sandmanlukas@gmail.com>
2022-08-23 18:27:40 +0200

    it seems to be working, needs more testing and documentation

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,9 @@
-use std::error::Error;
-use std::{thread, time, io::{stdout, Write}};
-
+use std::{error::Error,thread::sleep, time::Duration};
+use terminal_spinners::{SpinnerBuilder, DOTS};
 
 use headless_chrome::Browser;
 use headless_chrome::LaunchOptionsBuilder;
-//use headless_chrome::Element;
-
+use headless_chrome::Element;
 
 pub fn browser() -> Browser {
     Browser::new(
@@ -18,51 +16,150 @@ pub fn browser() -> Browser {
 }
 
 
-pub fn fetch_data() -> Result<(), Box<dyn Error>> {
+fn get_str_from_element(elem: Result<Element, Box<dyn Error>>) -> String {
+    match elem {
+        Ok(elem) => {
+            let val = elem.call_js_fn("function() { return this.textContent;}", vec![], false).unwrap().value.unwrap();
+            let text = val.as_str().unwrap().trim().replace("\"","");
+
+            if text == "0" {
+                return "-".to_string();
+            } else {
+                return text;
+            }
+
+        },
+        Err(_e) => return String::new(),
+    }
+}
+
+
+pub fn fetch_data(more: bool) -> Result<(), Box<dyn Error>> {
     let browser = browser();
 
     let tab = browser.wait_for_initial_tab()?;
-    tab.set_default_timeout(std::time::Duration::from_secs(60));
 
     tab.navigate_to("https://fast.com")?;
 
-    let hundred_millis = time::Duration::from_millis(100);
-
-    //let mut stdout = stdout();
+    let hundred_millis = Duration::from_millis(100);
+    let handle = SpinnerBuilder::new().spinner(&DOTS).text("").start();
 
     loop {
 
-        let down_elem = tab.find_element("#speed-value")?;
+        // Add some sort of error check here, if the loop has passed x amounts of time, break.
 
-        let down_unit_str = match tab.find_element("#speed-units") {
-            Ok(elem)=> {
-                let val = elem.call_js_fn("function() { return this.textContent;}",vec![], false)?.value.unwrap();
-                val.as_str().unwrap().trim().replace("\"","")
+        let down_speed_elem = match tab.find_element("#speed-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let up_speed_elem = match tab.find_element("#upload-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let down_units_elem = match tab.find_element("#speed-units") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let up_units_elem = match tab.find_element("#upload-units") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let latency_units_elem = match tab.find_element("#latency-units") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let bufferbloat_units_elem = match tab.find_element("#bufferbloat-units") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let downloaded_elem = match tab.find_element("#down-mb-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let uploaded_elem = match tab.find_element("#up-mb-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let latency_elem = match tab.find_element("#latency-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let buffer_bloat_elem = match tab.find_element("#bufferbloat-value") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+
+        let location_elem = match tab.find_element("#user-location") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let server_location_elem = match tab.find_element("#server-locations") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let ip_elem = match tab.find_element("#user-ip") {
+            Ok(elem) => elem,
+            Err(_e) => continue,
+        };
+
+        let down_speed = get_str_from_element(Ok(down_speed_elem));
+        let up_speed = get_str_from_element(Ok(up_speed_elem));
+        let down_unit = get_str_from_element(Ok(down_units_elem));
+        let up_unit = get_str_from_element(Ok(up_units_elem));
+        let latency_unit = get_str_from_element(Ok(latency_units_elem));
+        let bufferbloat_unit = get_str_from_element(Ok(bufferbloat_units_elem));
+        let downloaded = get_str_from_element(Ok(downloaded_elem));
+        let uploaded = get_str_from_element(Ok(uploaded_elem));
+        let latency = get_str_from_element(Ok(latency_elem));
+        let buffer_bloat = get_str_from_element(Ok(buffer_bloat_elem));
+        let location = get_str_from_element(Ok(location_elem));
+        let server_location = get_str_from_element(Ok(server_location_elem));
+        let ip = get_str_from_element(Ok(ip_elem));
+
+
+
+        let terminal_str = format!(" {} {} ↓ / {} {} ↑", down_speed, down_unit, up_speed, up_unit);
+        handle.text(terminal_str);
+
+        match (tab.find_element("#speed-value.succeeded"),tab.find_element("#upload-value.succeeded")) {
+            (Ok(_down), Ok(_up)) =>  {
+                let closed = tab.close(true)?;
+                assert_eq!(closed, true);
+
+                handle.done();
+
+                if more {
+                    println!("Downloaded: {} MB", downloaded);
+                    println!("Uploaded: {} MB", uploaded);
+                    println!("Latency: {} {}", latency, latency_unit);
+                    println!("Bufferbloat: {} {}", buffer_bloat, bufferbloat_unit);
+                    println!("Client: {} {}",ip, location);
+                    println!("Server/servers: {}", server_location);
+                }
+
+                break
             },
-            Err(_e) => String::new(),
+
+            _ => (),
         };
 
-        let down_val = down_elem.call_js_fn("function() { return this.textContent;}",vec![], false)?.value.unwrap();
-        //let down_unit_val = down_unit_elem.call_js_fn("function() { return this.textContent;}",vec![], false)?.value.unwrap();
-
-        let down_str = down_val.as_str().unwrap().trim().replace("\"", "");
-        //let down_unit_str = down_unit_val.as_str().unwrap().trim().replace("\"","");
-
-
-        print!("\rdown: {} {}", down_str, down_unit_str);
-        stdout().flush().unwrap();
-        //println!("up: {:#?}", up_speed);
-
-        match tab.find_element("#speed-value.succeeded") {
-            Ok(_done) =>  break,
-            Err(_e) => (),
-        };
-
-        thread::sleep(hundred_millis);
+        sleep(hundred_millis);
 
 
     }
-    println!();
+
     Ok(())
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod api;
 
+use clap::Parser;
+
+#[derive(Parser, Debug, Default)]
+#[clap(author="Lukas Sandman", version="1.0", about="CLI-app to measure internet speed among other things, using data from fast.com.", long_about = None)]
+struct Cli {
+    /// Display more information such as latency, ip-address, client location, server location, and more.
+    #[clap(short, long, takes_value = false)]
+    more: bool
+
+}
 
 fn main(){
+    let args = Cli::parse();
+    let more = args.more;
 
-   match api::fetch_data() {
-       Ok(()) => println!("done!"),
-       Err(e) => panic!("error e: {}", e),
-   }
+    match api::fetch_data(more) {
+        Ok(()) => (),
+        Err(e) => panic!("{}",e),
+    };
 
 }
